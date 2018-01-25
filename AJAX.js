39e76def65364acd4fb97232556dfe6152b25fa7
Ibrahim <ibinshikh@hotmail.com>
2018-01-26 00:39:02 +0300

    New Release
    Added support for multiple callbacks. Enable or disable a callback given
    its ID. Remove a callback given its ID. AJAX for old browsers (IE) Bug
    fixes.

--- AJAX.js
+++ AJAX.js
@@ -1,8 +1,22 @@
 
 "use strict";
 /**
+ * Names of pools of events.
+ * @type Array
+ */
+AJAX.CALLBACK_POOLS = ['servererror','clienterror','success'];
+/**
+ * Array of functions used to create XMLHttpRequest object.
+ * @type Array
+ */
+AJAX.XMLHttpFactories = [
+    function (){return new XMLHttpRequest();},
+    function (){return new ActiveXObject("Microsoft.XMLHTTP");},
+    function (){return new ActiveXObject("MSXML2.XMLHTTP.3.0");}
+];
+/**
  * A class that can be used to simplfy AJAX requests.
- * @version 0.0.1
+ * @version 0.0.2
  * @author Ibrahim BinAlshikh <ibinshikh@hotmail.com>
  * @constructor
  * @returns {AJAX}
@@ -43,21 +57,39 @@ function AJAX(){
     /**
      * A pool of functions to call in case of successful request.
      */
-    this.onsuccesspool = [function(code=''){
-        console.info('AJAX: Success '+code);
-    }];
+    this.onsuccesspool = [
+        {
+            'id':0,
+            'call':true,
+            'func':function(code=''){
+                console.info('AJAX: Success '+code);
+            }
+        }
+    ];
     /**
      * A pool of functions to call in case of server error.
      */
-    this.onservererrorpool = [function(code=''){
-        console.info('AJAX: Server Error '+code);
-    }];
+    this.onservererrorpool = [
+        {
+            'id':0,
+            'call':true,
+            'func':function(code=''){
+                console.info('AJAX: Server Error '+code);
+            }
+        }
+    ];
     /**
      * A pool of functions to call in case of client error.
      */
-    this.onclienterrorpool = [function(code=''){
-        console.info('AJAX: Client Error '+code);
-    }];
+    this.onclienterrorpool = [
+        {
+            'id':0,
+            'call':true,
+            'func':function(code=''){
+                console.info('AJAX: Client Error '+code);
+            }
+        }
+    ];
     this.onreadystatechange = function(){
         if(this.readyState === 0){
             console.info('AJAX: Ready State = 0 (UNSENT)');
@@ -74,13 +106,17 @@ function AJAX(){
         else if(this.readyState === 4 && this.status >= 200 && this.status < 300){
             console.info('AJAX: Ready State = 4 (DONE)');
             for(var i = 0 ; i < this.onsuccesspool.length ; i++){
-                this.onsuccesspool[i](this.status);
+                if(this.onsuccesspool[i].call === true){
+                    this.onsuccesspool[i].func(this.status);
+                }
             }
         }
         else if(this.readyState === 4 && this.status >= 400 && this.status < 500){
             console.info('AJAX: Ready State = 4 (DONE)');
-            for(var i = 0 ; i < this.onsuccesspool.length ; i++){
-                this.onclienterrorpool[i](this.status);
+            for(var i = 0 ; i < this.onclienterrorpool.length ; i++){
+                if(this.onclienterrorpool[i].call === true){
+                    this.onclienterrorpool[i].func(this.status);
+                }
             }
         }
         else if(this.readyState === 4 && this.status >= 300 && this.status < 400){
@@ -90,7 +126,9 @@ function AJAX(){
         else if(this.readyState === 4 && this.status >= 500 && this.status < 600){
             console.info('AJAX: Ready State = 4 (DONE)');
             for(var i = 0 ; i < this.onsuccesspool ; i++){
-                this.onservererrorpool[i](this.status);
+                if(this.onsuccesspool[i].call === true){
+                    this.onsuccesspool[i].func(this.status);
+                }
             }
         }
         else if(this.readyState === 4){
@@ -103,12 +141,9 @@ function AJAX(){
      * return XHR object that can be used to send AJAX.
      */
     function createXhr(){
-        var XMLHttpFactories = [
-            function () {return new XMLHttpRequest();}
-        ];
-        for(var i = 0 ; i < XMLHttpFactories.length ; i++){
+        for(var i = 0 ; i < AJAX.XMLHttpFactories.length ; i++){
             try{
-                return XMLHttpFactories[i]();
+                return AJAX.XMLHttpFactories[i]();
             }
             catch(e){
 
@@ -116,6 +151,30 @@ function AJAX(){
         }
         return false;
     };
+    /**
+     * A utility function used to show warning in the console about the existance 
+     * of events pool.
+     * @param {String} p_name The name of the pool.
+     * @returns {undefined}
+     */
+    function noSuchPool(p_name){
+        console.warn('No such bool: '+p_name);
+        var pools = '';
+        for(var x = 0 ; x < AJAX.CALLBACK_POOLS.length ; x++){
+            if(x === AJAX.CALLBACK_POOLS.length - 1){
+               pools += ' or '+ AJAX.CALLBACK_POOLS[x];
+            }
+            else{
+                if(x === AJAX.CALLBACK_POOLS.length - 2){
+                   pools += AJAX.CALLBACK_POOLS[x]; 
+                }
+                else{
+                    pools += AJAX.CALLBACK_POOLS[x]+', ';
+                }
+            }
+        }
+        console.info('Pool name must be one of the following: '+pools);
+    }
     /**
      * Sets the value of the property serverResponse. Do not call this function 
      * manually.
@@ -151,27 +210,175 @@ function AJAX(){
      * Append a function to the pool of functions that will be called in case of 
      * server error (code 5xx). 
      * @param {Function} callback A function to call on server error. If this 
+     * @param {Boolean} call If true, the method will be called. Else if i i false,
+     * the method will be not called.
      * parameter is not a function, a warning will be shown in the console.
-     * @returns {undefined}
+     * @returns {undefined|Number} Returns an ID for the function. If not added, 
+     * the method will return undefined.
      */
-    this.setOnServerError = function(callback){
+    this.setOnServerError = function(callback,call=true){
         if(typeof callback === 'function'){
-            this.onservererrorpool.push(callback);
+            var id = this.onservererrorpool[this.onservererrorpool.length - 1]['id'] + 1; 
+            this.onservererrorpool.push({'id':id,'call':call,'func':callback});
+            return id;
         }
         else{
             console.warn('Provided parameter is not a function.');
         }
     };
     /**
-     * Append a function to the pool of functions that will be called in case of 
-     * client error (code 4xx). 
-     * @param {Function} callback A function to call on client error. If this 
-     * parameter is not a function, a warning will be shown in the console.
+     * Removes a callback function from a specific pool given its ID.
+     * @param {String} pool_name The name of the pool. It should be one of the 
+     * values in the array AJAX.CALLBACK_POOLS.
+     * @param {Number} id The ID of the callback function.
      * @returns {undefined}
      */
-    this.setOnClientError = function(callback){
+    this.removeCall = function(pool_name,id){
+        if(pool_name !== undefined && pool_name !== null){
+            if(typeof pool_name === 'string'){
+                pool_name = pool_name.toLowerCase();
+                if(AJAX.CALLBACK_POOLS.indexOf(pool_name) !== -1){
+                    pool_name = 'on'+pool_name+'pool';
+                    for(var x = 0 ; x < this[pool_name].length ; x++){
+                        if(this[pool_name][x]['id'] === id){
+                            this[pool_name].pop(this[pool_name][x]);
+                            return;
+                        }
+                    }
+                    console.warn('No callback was found with ID = '+id);
+                }
+                else{
+                    noSuchPool(pool_name);
+                }
+            }
+            else{
+                console.warn('Invalid pool name type. Pool name must be string.');
+            }
+        }
+        else{
+            noSuchPool(pool_name);
+        }
+    };
+    /**
+     * Disable all callback functions except the one that its ID is given.
+     * @param {String} pool_name The name of the pool. It should be a value from 
+     * the array AJAX.CALLBACK_POOLS.
+     * @param {Number} id The ID of the function that was provided when the function 
+     * was added to the pool. If the ID does not exist, All callbacks will be disabled.
+     * @returns {undefined}
+     */
+    this.disableCallExcept = function(pool_name,id){
+        if(pool_name !== undefined && pool_name !== null){
+            if(typeof pool_name === 'string'){
+                pool_name = pool_name.toLowerCase();
+                if(AJAX.CALLBACK_POOLS.indexOf(pool_name) !== -1){
+                    pool_name = 'on'+pool_name+'pool';
+                    for(var x = 0 ; x < this[pool_name].length ; x++){
+                        if(this[pool_name][x]['id'] !== id){
+                            this[pool_name][x]['call'] = false;
+                        }
+                        else{
+                            this[pool_name][x]['call'] = true;
+                        }
+                    }
+                }
+                else{
+                    noSuchPool(pool_name);
+                }
+            }
+            else{
+                console.warn('Invalid pool name type. Pool name must be string.');
+            }
+        }
+        else{
+            noSuchPool(pool_name);
+        }
+    },
+    /**
+     * Enable or disable a callback on specific pool.
+     * @param {String} pool_name The name of the pool. It must be one of the 
+     * values in the aray AJAX.CALLBACK_POOLS.
+     * @param {Number} id The ID of the callback. It is given when the callback 
+     * was added.
+     * @param {Boolean} call If set to true, the function will be called. Else 
+     * if it is set to false, it will be not called.
+     * @returns {undefined}
+     */
+    this.setCallEnabled = function(pool_name,id,call=true){
+        if(pool_name !== undefined && pool_name !== null){
+            if(typeof pool_name === 'string'){
+                pool_name = pool_name.toLowerCase();
+                if(AJAX.CALLBACK_POOLS.indexOf(pool_name) !== -1){
+                    pool_name = 'on'+pool_name+'pool';
+                    for(var x = 0 ; x < this[pool_name].length ; x++){
+                        if(this[pool_name][x]['id'] === id){
+                            this[pool_name][x]['call'] = call;
+                            return;
+                        }
+                    }
+                    console.warn('No callback was found with ID = '+id);
+                }
+                else{
+                    noSuchPool(pool_name);
+                }
+            }
+            else{
+                console.warn('Invalid pool name type. Pool name must be string.');
+            }
+        }
+        else{
+            noSuchPool(pool_name);
+        }
+    };
+    /**
+     * Returns an object that contains the information of a callback function. 
+     * @param {type} pool_name The name of the pool. It must be in the array 
+     * AJAX.CALLBACK_POOLS.
+     * @param {Number} id The ID of the callback.
+     * @returns {Object|undefined} Returns an object that contains the 
+     * information of the callback. If it is not found, or the pool name is invalid, 
+     * the method will show a warning in the console and returns undefined.
+     */
+    this.getCallBack = function(pool_name='',id){
+        if(pool_name !== undefined && pool_name !== null){
+            if(typeof pool_name === 'string'){
+                pool_name = pool_name.toLowerCase();
+                if(AJAX.CALLBACK_POOLS.indexOf(pool_name) !== -1){
+                    pool_name = 'on'+pool_name+'pool';
+                    for(var x = 0 ; x < this[pool_name].length ; x++){
+                        if(this[pool_name][x]['id'] === id){
+                            return this[pool_name][x];
+                        }
+                    }
+                    console.warn('No callback was found with ID = '+id);
+                }
+                else{
+                    noSuchPool(pool_name);
+                }
+            }
+            else{
+                console.warn('Invalid pool name type. Pool name must be string.');
+            }
+        }
+        else{
+            noSuchPool(pool_name);
+        }
+    };
+    /**
+     * Append a function to the pool of functions that will be called in case of 
+     * client error (code 4xx). 
+     * @param {Boolean} call If true, the method will be called. Else if i i false,
+     * the method will be not called.
+     * @param {Function} callback A function to call on client error. If this 
+     * parameter is not a function, a warning will be shown in the console.
+     * @returns {undefined|Number} Returns an ID for the function. If not added, 
+     * the method will return undefined.
+     */
+    this.setOnClientError = function(callback,call=true){
         if(typeof callback === 'function'){
-            this.onclienterrorpool.push(callback);
+            var id = this.onclienterrorpool[this.onclienterrorpool.length - 1]['id'] + 1; 
+            this.onclienterrorpool.push({'id':id,'call':call,'func':callback});
+            return id;
         }
         else{
             console.warn('Provided parameter is not a function.');
@@ -180,13 +387,18 @@ function AJAX(){
     /**
      * Append a function to the pool of functions that will be called in case of 
      * successfull request (code 2xx). 
+     * @param {Boolean} call If true, the method will be called. Else if i i false,
+     * the method will be not called.
      * @param {Function} callback A function to call on server error. If this 
      * parameter is not a function, a warning will be shown in the console.
-     * @returns {undefined}
+     * @returns {undefined|Number} Returns an ID for the function. If not added, 
+     * the method will return undefined.
      */
-    this.setOnSuccess = function(callback){
+    this.setOnSuccess = function(callback,call=true){
         if(typeof callback === 'function'){
-            this.onsuccesspool.push(callback);
+            var id = this.onsuccesspool[this.onsuccesspool.length - 1]['id'] + 1; 
+            this.onsuccesspool.push({'id':id,'call':call,'func':callback});
+            return id;
         }
         else{
             console.warn('Provided parameter is not a function.');
@@ -269,8 +481,13 @@ function AJAX(){
                 var instance = this;
                 var a = function(){
                     instance.setResponse(xhr.responseText);
+                    instance.removeCall('onservererror',id1);
+                    instance.removeCall('onsuccess',id2);
+                    instance.removeCall('onclienterror',id3);
                 };
-                this.setOnSuccess(a);
+                var id1 = this.setOnSuccess(a);
+                var id2 = this.setOnServerError(a);
+                var id3 = this.setOnClientError(a);
                 xhr.onreadystatechange = this.onreadystatechange;
                 xhr.onload = this.onload;
                 xhr.onprogress = this.onprogress;
