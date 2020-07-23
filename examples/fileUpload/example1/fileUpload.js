30436c784e373dcf803c964881db240be1e40b91
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2020-07-24 00:10:23 +0300

    Updates

--- examples/fileUpload/example1/fileUpload.js
+++ examples/fileUpload/example1/fileUpload.js
@@ -7,13 +7,11 @@ window.ajax = new AJAXRequest({
     
     
     method:'post',
-    beforeAjax:[
-        function () {
-            // We get the form from the document and convert it to 
-            // FormData object. This one is useful if we would like 
-            //to supply extra inputs with the file.
-            var form = document.getElementById('file-upload-form');
-            this.AJAXRequest.setParams(new FormData(form));
-        }
-    ]
+    beforeAjax: function () {
+        // We get the form from the document and convert it to 
+        // FormData object. This one is useful if we would like 
+        //to supply extra inputs with the file.
+        var form = document.getElementById('file-upload-form');
+        this.AJAXRequest.setParams(new FormData(form));
+    }
 });
