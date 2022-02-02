ba44d1b5ed66a61d79356c921f51225df321a08a
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-02-02 16:09:40 +0300

    Updated code samples.

--- examples/fileUpload/example1/fileUpload.js
+++ examples/fileUpload/example1/fileUpload.js
@@ -1,17 +1,12 @@
-window.ajax = new AJAXRequest({
-    verbose:true,
-    
-    //The script which is used to handle uploads.
-    //Modify this to pint to the actual uploads script.
-    url:'http://example.com/ajax/upload.php',
-    
-    
-    method:'post',
-    beforeAjax: function () {
-        // We get the form from the document and convert it to 
-        // FormData object. This one is useful if we would like 
-        //to supply extra inputs with the file.
-        var form = document.getElementById('file-upload-form');
-        this.AJAXRequest.setParams(new FormData(form));
-    }
+ajax.verbose = true;
+//The script which is used to handle uploads.
+//Modify this to pint to the actual uploads script.
+ajax.setURL('http://example.com/ajax/upload.php');
+ajax.setMethod('post');
+ajax.setBeforeAjax(function () {
+    // We get the form from the document and convert it to 
+    // FormData object. This one is useful if we would like 
+    //to supply extra inputs with the file.
+    var form = document.getElementById('file-upload-form');
+    this.AJAXRequest.setParams(new FormData(form));
 });
--- examples/get/getRequest.js
+++ examples/get/getRequest.js
@@ -1,84 +1,76 @@
-//Creates new instance of the class 'AJAXRequest' using specific configuration.
-//The configuration is supplied as an object.
-window.ajax = new AJAXRequest({
-    
-    //Show more messages in the console. Activate in development mode only.
-    verbose:true,
-    
-    // The end pont at which that will receive the request.
-    url:'https://packagist.org/packages/list.json',
-    
-    //The method which is used to call the end point.
-    method:'get',
-    
-    //Adding one callback which will get executed before AJAX 
-    //Request is sent. The callback in this case is 
-    //Used to collect user input and validate it.
-    //If it is invalid, intercept the request and disable AJAX.
-    //If valid, add it as request parameter and continue.
-    //Notice how we accessed the methods of the instance using 'this.AJAXRequest'.
-    beforeAjax: function () {
-        var list = document.getElementById('packages-list');
-        window.myList = list;
-        var vendorName = document.getElementById('package-name-input').value.trim();
-        if (vendorName.length === 0) {
-            this.AJAXRequest.setEnabled(false); 
-            list.innerHTML = '<li>Please enter vendor name.</li>';
-            return;
-        } else {
-            this.AJAXRequest.setEnabled(true);
-        }
 
-        list.innerHTML = '<li>Please wait a moment...</li>';
-        this.AJAXRequest.setParams({
-            vendor:vendorName
-        });
-    },
-    
-    //Add one callback which will get executed after ajax 
-    //request completed with success status (2xx or 3xx). 
-    onSuccess: function () {
-        alert('Fetched.');
-        var packages = this.jsonResponse.packageNames;
+//Show more messages in the console. Activate in development mode only.
+ajax.verbose = true;
 
-        var listBody = '';
-        for (var x = 0 ; x < packages.length ; x++) {
-            var pkgName = packages[x];
-            listBody += '<li><a href="https://packagist.org/packages/'+pkgName+'">'+pkgName+'</a></li>';
-        }
-        if (listBody.length === 0) {
-            listBody = '<li>NO Packages found.';
-        }
-        window.myList.innerHTML = listBody;
-    }, 
-    
-    //Adding two callbacks which will get executed after ajax 
-    //request completed with client error status (4xx). 
-    //What we do is simply display the response code of the server.
-    onClientErr:[ 
-        function () {
-            window.myList.innerHTML = '<li>'+this.status+'</li>';
-        },
-        function () {
-            alert('Error: '+this.status);
-        }
-    ],
-    
-    //One callback which will get executed after ajax 
-    //request completed with server error status (5xx). 
-    //What we do is simply display the response code of the server.
-    onServerErr: function () {
-        window.myList.innerHTML = '<li>'+this.status+'</li>';
-    },
-    
-    //An array that contains two callbacks which will get executed if no 
-    //internet connection is available
-    onDisconnected: [
-        function () {
-            window.myList.innerHTML = '<li>No internet access.</li>';
-        },
-        function () {
-            alert('Make sure that you are connected to the internet. (code '+this.status+')');
-        }
-    ]
+// The end pont at which that will receive the request.
+ajax.setURL('https://packagist.org/packages/list.json');
+
+//The method which is used to call the end point.
+ajax.setMethod('get');
+
+//Adding one callback which will get executed before AJAX 
+//Request is sent. The callback in this case is 
+//Used to collect user input and validate it.
+//If it is invalid, intercept the request and disable AJAX.
+//If valid, add it as request parameter and continue.
+//Notice how we accessed the methods of the instance using 'this.AJAXRequest'.
+ajax.setBeforeAjax(function () {
+    var list = document.getElementById('packages-list');
+    window.myList = list;
+    var vendorName = document.getElementById('package-name-input').value.trim();
+    if (vendorName.length === 0) {
+        this.AJAXRequest.setEnabled(false); 
+        list.innerHTML = '<li>Please enter vendor name.</li>';
+        return;
+    } else {
+        this.AJAXRequest.setEnabled(true);
+    }
+
+    list.innerHTML = '<li>Please wait a moment...</li>';
+    this.AJAXRequest.setParams({
+        vendor:vendorName
+    });
+});
+
+//Add one callback which will get executed after ajax 
+//request completed with success status (2xx or 3xx). 
+ajax.setOnSuccess(function () {
+    alert('Fetched.');
+    var packages = this.jsonResponse.packageNames;
+
+    var listBody = '';
+    for (var x = 0 ; x < packages.length ; x++) {
+        var pkgName = packages[x];
+        listBody += '<li><a href="https://packagist.org/packages/'+pkgName+'">'+pkgName+'</a></li>';
+    }
+    if (listBody.length === 0) {
+        listBody = '<li>NO Packages found.';
+    }
+    window.myList.innerHTML = listBody;
+});
+
+//Adding two callbacks which will get executed after ajax 
+//request completed with client error status (4xx). 
+//What we do is simply display the response code of the server.
+ajax.setOnClientError(function () {
+    window.myList.innerHTML = '<li>'+this.status+'</li>';
+});
+ajax.setOnClientError(function () {
+    alert('Error: '+this.status);
+});
+
+//One callback which will get executed after ajax 
+//request completed with server error status (5xx). 
+//What we do is simply display the response code of the server.
+ajax.setOnServerError(function () {
+    window.myList.innerHTML = '<li>'+this.status+'</li>';
+});
+
+//An array that contains two callbacks which will get executed if no 
+//internet connection is available
+ajax.setOnDisconnected(function () {
+    window.myList.innerHTML = '<li>No internet access.</li>';
+});
+ajax.setOnDisconnected(function () {
+    alert('Make sure that you are connected to the internet. (code '+this.status+')');
 });
